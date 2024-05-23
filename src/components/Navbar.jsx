2d52dd24f22f45f7f40e7b01fa332e7a3ea92a9d
Ayush Ambasta <ayushambasta2001@gmail.com>
2024-05-23 12:06:02 +0530

    navbar ui changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,8 +18,9 @@ function Navbar(){
   }
   return (
     <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6 justify-between">
-        <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
         <h1 className="text-lg font-bold">Promonade</h1>
+        <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
+        
           <Link
             to="/"
             className="text-muted-foreground transition-colors hover:text-foreground"
