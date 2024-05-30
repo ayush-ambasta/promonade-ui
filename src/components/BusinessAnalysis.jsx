f3059940ecd96953500d4c21c0340924a43a2ca8
Ayush Ambasta <ayushambasta2001@gmail.com>
2024-05-30 17:26:17 +0530

    analytics ui made responsive

--- src/components/BusinessAnalysis.jsx
+++ src/components/BusinessAnalysis.jsx
@@ -8,8 +8,10 @@ import { Coins, Wallet, Banknote, PersonStanding, Gem, LampDesk, BriefcaseBusine
 import { DatePickerWithRange } from "./DateRangePicker";
 import { addDays, format } from "date-fns"
 import { Button } from "./ui/button";
-
+import { useMediaQuery } from 'react-responsive';
 const BusinessAnalysis = () => {
+    const isSmallScreen = useMediaQuery({ maxWidth: 767 });
+    const isLargeScreen = useMediaQuery({ minWidth: 768 });
     const revenueData = {
         dates: ["2024-04-25T08:18:02.260+00:00", "2024-04-26T08:18:02.260+00:00", "2024-04-27T08:18:02.260+00:00", "2024-04-28T08:18:02.260+00:00", "2024-04-29T08:18:02.260+00:00"],
         revenues: [500, 200, 250, 170, 400]
@@ -85,23 +87,23 @@ const BusinessAnalysis = () => {
 
 
     return (
-        <div className="w-[125%]">
+        <div className="w-full lg:w-[125%] my-3">
             <div className="flex items-center my-4 mt-0">
                 <h1  className=" p-3 font-normal text-sm text-slate-500"> Your Analytics For:  &nbsp;&nbsp;&nbsp; <span className="text-2xl text-slate-700 font-normal">Your  Business </span></h1>
                 <BriefcaseBusiness size={35}/>
             </div>
-            <div className="p-4 bg-white rounded-lg w-fit mx-auto flex items-center gap-5">
+            <div className="p-4 bg-white rounded-lg w-full lg:w-fit mx-auto flex flex-col lg:flex-row items-center gap-5">
                 <h4 className="text-sm text-slate-600">Analysis Period</h4>
                 <DatePickerWithRange date={date} setDate={setDate} />
                 <Button className="h-8" onClick={performAnalysis}>Analyse</Button>
             </div>
             <div>
-                <div className="flex gap-6">
+                <div className="flex flex-col lg:flex-row gap-6 justify-around items-center">
                     <div id="line-graph-logins-vs-time" className="bg-white rounded-2xl p-6 w-fit my-5">
                         <div className="p-4 pt-0">
                             <h3 className="text-slate-500 font-normal text-lg">Logins Vs Time Analytics</h3>
                         </div>
-                        <LineChart width={650} height={200} data={businessLoginData} className="text-xs" >
+                        <LineChart width={isSmallScreen ? 430 : isLargeScreen ? 600 : 650} height={200} data={businessLoginData} className="text-xs" >
                             <CartesianGrid strokeDasharray="3 3" />
                             <XAxis dataKey="date" />
                             <YAxis />
@@ -110,7 +112,7 @@ const BusinessAnalysis = () => {
                             <Line type="monotone" dataKey="logins" stroke="#8884d8" />
                         </LineChart>
                     </div>
-                    <div className="flex flex-col justify-around">
+                    <div className="flex flex-col gap-3 w-full lg:w-fit">
                         <div id="purchase-conversion-rate" className="flex h-2/5 bg-white rounded-2xl items-center gap-5 p-5 justify-around">
                             <div className="rounded-full bg-green-100 p-2">
                                 <Wallet/>
@@ -133,7 +135,7 @@ const BusinessAnalysis = () => {
                         </div>
                     </div>
                 </div>
-                <div className="flex justify-around gap-8">
+                <div className="flex flex-col lg:flex-row justify-around gap-8 items-center">
                     <div id="bar-graph-revenue-vs-time" className="bg-white rounded-2xl p-6 w-fit my-5">
                         <div className="p-4 pt-0">
                             <h3 className="text-slate-500 font-normal text-lg">Revenue Vs Time Analytics</h3>
@@ -163,7 +165,7 @@ const BusinessAnalysis = () => {
                         <div className="p-4 pt-0">
                             <h3 className="text-slate-500 font-normal text-lg">Purchases Vs Time Analytics</h3>
                         </div>
-                        <LineChart width={400} height={300} data={businessPurchaseData} className="text-xs" >
+                        <LineChart width={430} height={300} data={businessPurchaseData} className="text-xs" >
                             <CartesianGrid strokeDasharray="3 3" />
                             <XAxis dataKey="date" />
                             <YAxis />
