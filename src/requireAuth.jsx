838c4eab5b3822e5f826726a7ecdf01254f0bb9b
Shades-en <owaisiqbal2013@gmail.com>
2024-06-04 00:01:10 +0530

    token expiration change

--- src/requireAuth.jsx
+++ src/requireAuth.jsx
@@ -1,16 +1,35 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import {Navigate, useLocation } from "react-router-dom";
 import UserContext from "./contexts/UserContext";
+import { testUserToken } from "./services/userService";
+import { useNavigate } from 'react-router-dom';
 
 export const RequireAuth = ({children})=> {
-    const {state} = useContext(UserContext);
+    const {state, dispatch} = useContext(UserContext);
     const user = state?.user || null;
-    // console.log(user)
     const location = useLocation();
+    const navigate = useNavigate();
+    const [tokenValid, setTokenValid] = useState(false)
 
-    if(user === null) {
-        return <Navigate to={'/login'} state={{path: location.pathname}} />;
+    const checkToken = async () => {
+        try {
+            const valid = await testUserToken();
+            setTokenValid(valid)
+        } catch (err) {
+            if (err.message === "SESSION_EXPIRED") {
+                dispatch({ type: 'LOGOUT' });
+                navigate('/login')
+            }
+        }
+    };
+
+    useEffect(() => {
+        checkToken();
+    }, [dispatch]);
+    
+    if (user === null) {
+        return <Navigate to={'/login'} state={{ path: location.pathname }} />;
     }
 
-    return children;
+    return tokenValid ? children : null;
 }
